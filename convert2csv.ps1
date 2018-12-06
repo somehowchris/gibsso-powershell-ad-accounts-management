c52fa27df8b57be0beb10935de2ddf59600b081e
somehowchris <38954618+somehowchris@users.noreply.github.com>
2018-12-06 11:42:08 +0100

    Create convert2csv.ps1

---
+++ convert2csv.ps1
@@ -0,0 +1,2 @@
+[xml]$XamlDocument = Get-Contenct c:\Cars.xml
+$inputFile.Transaction.TXNHEAD | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | Set-Content -Path "c:\pstest\test.csv" -Encoding UTF8
