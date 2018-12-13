3d5b23a15d0e96c005b559632cb882eeee98cf1c
florianleimer <florianleimer@github.com>
2018-12-13 16:23:45 +0100

    ConvertXMLToCSV

--- Functions/ConvertXML2CSV.ps1
+++ Functions/ConvertXML2CSV.ps1
@@ -1,4 +1,18 @@
-function convert2CSV($path, $outpath) {
-  [xml]$XamlDocument = Get-Contenct $path
-  $XamlDocument | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | Set-Content -Path $outpath -Encoding UTF8
+﻿function convertXmlToCsv {
+    param (
+        [Parameter(mandatory=$true)] [String]$inputPath,
+        [Parameter(mandatory=$true)] [String]$outputPath,
+        [Parameter(mandatory=$false)] [String]$outputEncoding = "UTF8"
+    )
+    
+    # Read from file
+    [xml]$inputFile = Get-Content $inputPath
+
+    # Set separator in file
+    Set-Content -Value "sep=;" -Path $outputPath -Encoding $outputEncoding
+
+    # Export xml as csv
+    $inputFile.ad.schueler | ConvertTo-Csv -NoTypeInformation -Delimiter ";" | Add-Content -Path $outputPath -Encoding $outputEncoding
 }
+
+convertXmlToCsv -inputPath "$PSScriptRoot\..\Ressources\gibsso_AD-Export.xml" -outputPath "$PSScriptRoot\..\Ressources\gibsso_AD-Export.csv"
