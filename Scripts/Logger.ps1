aa8e841b0d6faa852b8ec410aadbb90549351743
Christof Weickhardt <>
2018-12-13 14:54:36 +0100

    updated log data

--- Scripts/Logger.ps1
+++ Scripts/Logger.ps1
@@ -1,4 +1,4 @@
 function log($message) {
   $dir = get-location | Select -Property Path
-  Write-Output "$(Get-TimeStamp) ${message}" | Out-file "${dir}/log.txt" -append
+  Write-Output "$(Get-TimeStamp) ${message}" | Out-file "${dir}/.log" -append
 }
