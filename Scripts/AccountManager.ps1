6c8868e5f8de65c32c118a64929fc1c519945e35
florianleimer <44466890+florianleimer@users.noreply.github.com>
2019-01-11 22:44:50 +0100

    Added some small bugfixes and features

--- Scripts/AccountManager.ps1
+++ Scripts/AccountManager.ps1
@@ -1,8 +1,9 @@
 function find-existingAccount() {}
-function create-Account([String] $name) {
-    New-ADUser -Name $name -AccountPassword $global:defaultPassword -path "OU=Lernende, DC=GIBS, DC=m122g, DC=local"
+function add-Account([String]$name, [String]$prename) {
+    # Does not work yet
+    New-ADUser -Organization $global:userOU -Name $name -AccountPassword (ConvertTo-SecureString -AsPlainText $global:defaultPassword -Force) -PassThru | Enable-ADAccount
 }
-function deactivate-Account() {}
-function activate-Account() {}
+function disable-Account() {}
+function enable-Account() {}
 function update-Account() {}
 function add-AccountToGroup() {}
