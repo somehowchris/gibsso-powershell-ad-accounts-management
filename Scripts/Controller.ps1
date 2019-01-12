65ade65e8cd64160935a84187065e962e43d07ec
Christof Weickhardt <38954618+somehowchris@users.noreply.github.com>
2019-01-12 16:24:33 +0100

    Updated Imports

--- Scripts/Controller.ps1
+++ Scripts/Controller.ps1
@@ -1,4 +1,5 @@
 . "$PSScriptRoot\Config.ps1"
+. "$PSScriptRoot\Logger.ps1"
 . "$PSScriptRoot\Managers\FileManager.ps1"
 . "$PSScriptRoot\Managers\AccountManager.ps1"
 . "$PSScriptRoot\Handlers\FlowHandler.ps1"
