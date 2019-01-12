49f17752ddb3d51482b932a231efe49f363cf966
florianleimer <44466890+florianleimer@users.noreply.github.com>
2019-01-12 18:19:26 +0100

    Deleted config.ps1

---
+++ Scripts/config.ps1
@@ -0,0 +1,8 @@
+$global:domain = "m122g.local"
+$global:mainOU = "GIBS"
+$global:userOU = "Lernende"
+$global:groupOU = "Klassengruppen"
+$global:defaultPassword = "gibs.001"
+$global:xmlPath = "$PSScriptRoot\..\Ressources\gibsso_AD-Export.xml"
+$global:csvPath = "$PSScriptRoot\..\Ressources\gibsso_AD-Export.csv"
+$global:encoding = "UTF8"
